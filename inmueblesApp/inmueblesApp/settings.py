db8bb746d069ffd2cc789ca191d75562e1455c5e
gonzalo olivares <g.olivaresca@gmail.com>
2024-05-13 21:16:28 -0400

    Creación y configuración de database más migraciones

--- inmueblesApp/inmueblesApp/settings.py
+++ inmueblesApp/inmueblesApp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'registro_inmuebles',
 ]
 
 MIDDLEWARE = [
@@ -74,9 +75,13 @@ WSGI_APPLICATION = 'inmueblesApp.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "inmueblesdb",
+        "USER": "golivares",
+        "PASSWORD": "mildred1000",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
